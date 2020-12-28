3df33d2be4f5655723cc842197794929e03e8a6c
filip20121 <50912416+filip20121@users.noreply.github.com>
2020-12-28 16:45:22 +0100

    Add files via upload

--- build-kahoot-Desktop-Debug/moc_client.cpp
+++ build-kahoot-Desktop-Debug/moc_client.cpp
@@ -22,7 +22,7 @@ QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_CLient_t {
     QByteArrayData data[4];
-    char stringdata0[34];
+    char stringdata0[27];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,12 +32,12 @@ struct qt_meta_stringdata_CLient_t {
 static const qt_meta_stringdata_CLient_t qt_meta_stringdata_CLient = {
     {
 QT_MOC_LITERAL(0, 0, 6), // "CLient"
-QT_MOC_LITERAL(1, 7, 20), // "on_port_valueChanged"
-QT_MOC_LITERAL(2, 28, 0), // ""
-QT_MOC_LITERAL(3, 29, 4) // "arg1"
+QT_MOC_LITERAL(1, 7, 11), // "resultReady"
+QT_MOC_LITERAL(2, 19, 0), // ""
+QT_MOC_LITERAL(3, 20, 6) // "result"
 
     },
-    "CLient\0on_port_valueChanged\0\0arg1"
+    "CLient\0resultReady\0\0result"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,13 +52,13 @@ static const uint qt_meta_data_CLient[] = {
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
 
- // slots: name, argc, parameters, tag, flags
-       1,    1,   19,    2, 0x08 /* Private */,
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x06 /* Public */,
 
- // slots: parameters
-    QMetaType::Void, QMetaType::Int,    3,
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
 
        0        // eod
 };
@@ -68,10 +68,19 @@ void CLient::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
     if (_c == QMetaObject::InvokeMetaMethod) {
         auto *_t = static_cast<CLient *>(_o);
         Q_UNUSED(_t)
-        /*switch (_id) {
-        case 0: _t->on_port_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        switch (_id) {
+        case 0: _t->resultReady((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         default: ;
-        }*/
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (CLient::*)(const QString & );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&CLient::resultReady)) {
+                *result = 0;
+                return;
+            }
+        }
     }
 }
 
@@ -113,6 +122,231 @@ int CLient::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         _id -= 1;
     }
     return _id;
+}
+
+// SIGNAL 0
+void CLient::resultReady(const QString & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+struct qt_meta_stringdata_Sender_t {
+    QByteArrayData data[3];
+    char stringdata0[16];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Sender_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Sender_t qt_meta_stringdata_Sender = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Sender"
+QT_MOC_LITERAL(1, 7, 7), // "operate"
+QT_MOC_LITERAL(2, 15, 0) // ""
+
+    },
+    "Sender\0operate\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Sender[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    2,
+
+       0        // eod
+};
+
+void Sender::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Sender *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->operate((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Sender::*)(const QString & );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Sender::operate)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Sender::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_Sender.data,
+    qt_meta_data_Sender,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Sender::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Sender::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Sender.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Sender::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Sender::operate(const QString & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+struct qt_meta_stringdata_Listener_t {
+    QByteArrayData data[3];
+    char stringdata0[18];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Listener_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Listener_t qt_meta_stringdata_Listener = {
+    {
+QT_MOC_LITERAL(0, 0, 8), // "Listener"
+QT_MOC_LITERAL(1, 9, 7), // "operate"
+QT_MOC_LITERAL(2, 17, 0) // ""
+
+    },
+    "Listener\0operate\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Listener[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    2,
+
+       0        // eod
+};
+
+void Listener::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Listener *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->operate((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Listener::*)(const QString & );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Listener::operate)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Listener::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_Listener.data,
+    qt_meta_data_Listener,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Listener::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Listener::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Listener.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Listener::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Listener::operate(const QString & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
--- build-kahoot-Desktop-Debug/ui_client.h
+++ build-kahoot-Desktop-Debug/ui_client.h
@@ -29,32 +29,35 @@ public:
     QTextEdit *quiz;
     QGroupBox *connectAnswer;
     QPushButton *send;
-    QWidget *widget;
+    QWidget *layoutWidget;
     QHBoxLayout *checkBoxes;
     QRadioButton *a;
     QRadioButton *b;
     QRadioButton *c;
     QRadioButton *d;
     QGroupBox *connectMenu;
-    QGroupBox *connectCeate;
+    QGroupBox *connectCreate;
     QPushButton *create;
     QSpinBox *code;
     QLabel *label_5;
     QSpinBox *numOfQuest;
     QLabel *label_6;
-    QGroupBox *connectJoin;
-    QPushButton *accept;
-    QLineEdit *server;
-    QLabel *label;
-    QLabel *label_2;
-    QSpinBox *code_join;
     QGroupBox *connectQuestion;
     QPushButton *add;
     QLineEdit *question;
     QLineEdit *correct;
     QLabel *label_7;
     QLabel *label_8;
-    QPushButton *startButton;
+    QGroupBox *connect;
+    QPushButton *connectButton;
+    QLineEdit *server;
+    QLabel *label;
+    QLabel *label_2;
+    QSpinBox *port;
+    QGroupBox *join_game;
+    QPushButton *joinButton;
+    QLabel *label_9;
+    QSpinBox *join_code;
     QPushButton *exitButton;
 
     void setupUi(QWidget *CLient)
@@ -64,91 +67,70 @@ public:
         CLient->resize(800, 689);
         quiz = new QTextEdit(CLient);
         quiz->setObjectName(QString::fromUtf8("quiz"));
-        quiz->setGeometry(QRect(20, 380, 681, 121));
+        quiz->setGeometry(QRect(20, 480, 681, 121));
         connectAnswer = new QGroupBox(CLient);
         connectAnswer->setObjectName(QString::fromUtf8("connectAnswer"));
-        connectAnswer->setGeometry(QRect(30, 510, 311, 71));
+        connectAnswer->setGeometry(QRect(30, 610, 311, 71));
         send = new QPushButton(connectAnswer);
         send->setObjectName(QString::fromUtf8("send"));
         send->setGeometry(QRect(200, 30, 89, 25));
-        widget = new QWidget(connectAnswer);
-        widget->setObjectName(QString::fromUtf8("widget"));
-        widget->setGeometry(QRect(20, 30, 161, 25));
-        checkBoxes = new QHBoxLayout(widget);
+        layoutWidget = new QWidget(connectAnswer);
+        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setGeometry(QRect(20, 30, 161, 25));
+        checkBoxes = new QHBoxLayout(layoutWidget);
         checkBoxes->setObjectName(QString::fromUtf8("checkBoxes"));
         checkBoxes->setContentsMargins(0, 0, 0, 0);
-        a = new QRadioButton(widget);
+        a = new QRadioButton(layoutWidget);
         a->setObjectName(QString::fromUtf8("a"));
 
         checkBoxes->addWidget(a);
 
-        b = new QRadioButton(widget);
+        b = new QRadioButton(layoutWidget);
         b->setObjectName(QString::fromUtf8("b"));
 
         checkBoxes->addWidget(b);
 
-        c = new QRadioButton(widget);
+        c = new QRadioButton(layoutWidget);
         c->setObjectName(QString::fromUtf8("c"));
 
         checkBoxes->addWidget(c);
 
-        d = new QRadioButton(widget);
+        d = new QRadioButton(layoutWidget);
         d->setObjectName(QString::fromUtf8("d"));
 
         checkBoxes->addWidget(d);
 
         connectMenu = new QGroupBox(CLient);
         connectMenu->setObjectName(QString::fromUtf8("connectMenu"));
-        connectMenu->setGeometry(QRect(20, 0, 681, 371));
-        connectCeate = new QGroupBox(connectMenu);
-        connectCeate->setObjectName(QString::fromUtf8("connectCeate"));
-        connectCeate->setGeometry(QRect(10, 120, 671, 101));
-        create = new QPushButton(connectCeate);
+        connectMenu->setGeometry(QRect(10, 10, 691, 461));
+        connectCreate = new QGroupBox(connectMenu);
+        connectCreate->setObjectName(QString::fromUtf8("connectCreate"));
+        connectCreate->setGeometry(QRect(10, 200, 671, 251));
+        create = new QPushButton(connectCreate);
         create->setObjectName(QString::fromUtf8("create"));
         create->setGeometry(QRect(560, 60, 91, 31));
-        code = new QSpinBox(connectCeate);
+        code = new QSpinBox(connectCreate);
         code->setObjectName(QString::fromUtf8("code"));
         code->setGeometry(QRect(250, 60, 111, 31));
         code->setMinimumSize(QSize(111, 31));
         code->setMinimum(0);
         code->setMaximum(9999);
-        label_5 = new QLabel(connectCeate);
+        label_5 = new QLabel(connectCreate);
         label_5->setObjectName(QString::fromUtf8("label_5"));
         label_5->setGeometry(QRect(250, 30, 67, 17));
-        numOfQuest = new QSpinBox(connectCeate);
+        numOfQuest = new QSpinBox(connectCreate);
         numOfQuest->setObjectName(QString::fromUtf8("numOfQuest"));
         numOfQuest->setGeometry(QRect(10, 60, 111, 31));
         numOfQuest->setMinimumSize(QSize(111, 31));
         numOfQuest->setMinimum(1);
         numOfQuest->setMaximum(10);
         numOfQuest->setValue(1);
-        label_6 = new QLabel(connectCeate);
+        label_6 = new QLabel(connectCreate);
         label_6->setObjectName(QString::fromUtf8("label_6"));
         label_6->setGeometry(QRect(10, 30, 161, 17));
-        connectJoin = new QGroupBox(connectMenu);
-        connectJoin->setObjectName(QString::fromUtf8("connectJoin"));
-        connectJoin->setGeometry(QRect(10, 30, 671, 91));
-        accept = new QPushButton(connectJoin);
-        accept->setObjectName(QString::fromUtf8("accept"));
-        accept->setGeometry(QRect(560, 50, 91, 31));
-        server = new QLineEdit(connectJoin);
-        server->setObjectName(QString::fromUtf8("server"));
-        server->setGeometry(QRect(10, 50, 271, 31));
-        label = new QLabel(connectJoin);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(10, 30, 67, 17));
-        label_2 = new QLabel(connectJoin);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(310, 30, 67, 17));
-        code_join = new QSpinBox(connectJoin);
-        code_join->setObjectName(QString::fromUtf8("code_join"));
-        code_join->setGeometry(QRect(310, 50, 111, 31));
-        code_join->setMinimumSize(QSize(111, 31));
-        code_join->setMinimum(0);
-        code_join->setMaximum(9999);
-        connectQuestion = new QGroupBox(connectMenu);
+        connectQuestion = new QGroupBox(connectCreate);
         connectQuestion->setObjectName(QString::fromUtf8("connectQuestion"));
-        connectQuestion->setGeometry(QRect(20, 230, 651, 141));
+        connectQuestion->setGeometry(QRect(10, 100, 671, 141));
         add = new QPushButton(connectQuestion);
         add->setObjectName(QString::fromUtf8("add"));
         add->setGeometry(QRect(550, 100, 91, 31));
@@ -164,12 +146,45 @@ public:
         label_8 = new QLabel(connectQuestion);
         label_8->setObjectName(QString::fromUtf8("label_8"));
         label_8->setGeometry(QRect(10, 30, 71, 17));
-        startButton = new QPushButton(CLient);
-        startButton->setObjectName(QString::fromUtf8("startButton"));
-        startButton->setGeometry(QRect(530, 570, 89, 25));
+        connect = new QGroupBox(connectMenu);
+        connect->setObjectName(QString::fromUtf8("connect"));
+        connect->setGeometry(QRect(10, 30, 671, 91));
+        connectButton = new QPushButton(connect);
+        connectButton->setObjectName(QString::fromUtf8("connectButton"));
+        connectButton->setGeometry(QRect(560, 50, 91, 31));
+        server = new QLineEdit(connect);
+        server->setObjectName(QString::fromUtf8("server"));
+        server->setGeometry(QRect(10, 50, 271, 31));
+        label = new QLabel(connect);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(10, 30, 67, 17));
+        label_2 = new QLabel(connect);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(310, 30, 67, 17));
+        port = new QSpinBox(connect);
+        port->setObjectName(QString::fromUtf8("port"));
+        port->setGeometry(QRect(310, 50, 111, 31));
+        port->setMinimumSize(QSize(111, 31));
+        port->setMinimum(0);
+        port->setMaximum(9999);
+        join_game = new QGroupBox(connectMenu);
+        join_game->setObjectName(QString::fromUtf8("join_game"));
+        join_game->setGeometry(QRect(10, 120, 671, 71));
+        joinButton = new QPushButton(join_game);
+        joinButton->setObjectName(QString::fromUtf8("joinButton"));
+        joinButton->setGeometry(QRect(560, 30, 91, 31));
+        label_9 = new QLabel(join_game);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+        label_9->setGeometry(QRect(10, 30, 71, 31));
+        join_code = new QSpinBox(join_game);
+        join_code->setObjectName(QString::fromUtf8("join_code"));
+        join_code->setGeometry(QRect(80, 30, 111, 31));
+        join_code->setMinimumSize(QSize(111, 31));
+        join_code->setMinimum(0);
+        join_code->setMaximum(9999);
         exitButton = new QPushButton(CLient);
         exitButton->setObjectName(QString::fromUtf8("exitButton"));
-        exitButton->setGeometry(QRect(620, 570, 89, 25));
+        exitButton->setGeometry(QRect(600, 660, 89, 25));
 
         retranslateUi(CLient);
 
@@ -186,7 +201,7 @@ public:
         c->setText(QApplication::translate("CLient", "C", nullptr));
         d->setText(QApplication::translate("CLient", "D", nullptr));
         connectMenu->setTitle(QApplication::translate("CLient", "Menu", nullptr));
-        connectCeate->setTitle(QApplication::translate("CLient", "Create", nullptr));
+        connectCreate->setTitle(QApplication::translate("CLient", "Create", nullptr));
         create->setText(QApplication::translate("CLient", "Create Quiz", nullptr));
 #ifndef QT_NO_TOOLTIP
         code->setToolTip(QApplication::translate("CLient", "<html><head/><body><p><br/></p></body></html>", nullptr));
@@ -196,21 +211,26 @@ public:
         numOfQuest->setToolTip(QApplication::translate("CLient", "<html><head/><body><p><br/></p></body></html>", nullptr));
 #endif // QT_NO_TOOLTIP
         label_6->setText(QApplication::translate("CLient", "Number of questions", nullptr));
-        connectJoin->setTitle(QApplication::translate("CLient", "Join", nullptr));
-        accept->setText(QApplication::translate("CLient", "Join", nullptr));
-        server->setText(QApplication::translate("CLient", "localhost", nullptr));
-        label->setText(QApplication::translate("CLient", "Server", nullptr));
-        label_2->setText(QApplication::translate("CLient", "Code", nullptr));
-#ifndef QT_NO_TOOLTIP
-        code_join->setToolTip(QApplication::translate("CLient", "<html><head/><body><p><br/></p></body></html>", nullptr));
-#endif // QT_NO_TOOLTIP
         connectQuestion->setTitle(QApplication::translate("CLient", "Add question", nullptr));
         add->setText(QApplication::translate("CLient", "Add", nullptr));
         question->setText(QString());
         correct->setText(QString());
         label_7->setText(QApplication::translate("CLient", "Correct answer", nullptr));
         label_8->setText(QApplication::translate("CLient", "Question", nullptr));
-        startButton->setText(QApplication::translate("CLient", "Start", nullptr));
+        connect->setTitle(QApplication::translate("CLient", "Connect to server", nullptr));
+        connectButton->setText(QApplication::translate("CLient", "Connect", nullptr));
+        server->setText(QApplication::translate("CLient", "localhost", nullptr));
+        label->setText(QApplication::translate("CLient", "Server", nullptr));
+        label_2->setText(QApplication::translate("CLient", "Port", nullptr));
+#ifndef QT_NO_TOOLTIP
+        port->setToolTip(QApplication::translate("CLient", "<html><head/><body><p><br/></p></body></html>", nullptr));
+#endif // QT_NO_TOOLTIP
+        join_game->setTitle(QApplication::translate("CLient", "Join to the game", nullptr));
+        joinButton->setText(QApplication::translate("CLient", "Join", nullptr));
+        label_9->setText(QApplication::translate("CLient", "Code", nullptr));
+#ifndef QT_NO_TOOLTIP
+        join_code->setToolTip(QApplication::translate("CLient", "<html><head/><body><p><br/></p></body></html>", nullptr));
+#endif // QT_NO_TOOLTIP
         exitButton->setText(QApplication::translate("CLient", "Exit", nullptr));
     } // retranslateUi
 
